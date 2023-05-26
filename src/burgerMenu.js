6956ad8b09a85c6ba6112abb4eec2d0aeac8aa2f
Andrea <andr45f9@stud.kea.dk>
2023-05-26 11:51:05 +0200

    burger menu done

--- src/burgerMenu.js
+++ src/burgerMenu.js
@@ -1,14 +1,18 @@
 import "./style.css";
 
+/* Toggle mellem at åbne og lukke burgermenuen */
 const menuBtn = document.querySelector("#burger_icon");
-let menuOpen = false;
-
 const menuLinks = document.querySelector("#burger_links");
 
 menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuLinks.style.display = "block";
-  } else {
-    menuLinks.style.display = "none";
-  }
+  menuLinks.classList.toggle("hidden");
+  menuBtn.classList.toggle("change");
+});
+
+/* Toggler mellem at åbne og lukke behandlingserne (så man kan se zoneterapi, japansk lifting osv.) */
+const treatmentLink = document.querySelector("#behandling_link");
+const treatmentBox = document.querySelector("#behandlings_boks");
+
+treatmentLink.addEventListener("click", () => {
+  treatmentBox.classList.toggle("hidden");
 });
