2047b95a1f1d7de20e779bc4fa60565974a363d3
Alberte <albe304d@stud.kea.dk>
2023-05-27 11:03:41 +0200

    klargjort til dist i vite.config

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         priser: resolve(__dirname, "src/priser.html"),
+        fysiurgiskmassage: resolve(__dirname, "src/fysiurgiskmassage.html"),
+        japansklifting: resolve(__dirname, "src/japansklifting.html"),
+        kombi: resolve(__dirname, "src/kombi.html"),
+        kontakt: resolve(__dirname, "src/kontakt.html"),
+        om: resolve(__dirname, "src/om.html"),
+        zoneterapi: resolve(__dirname, "src/zoneterapi.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
